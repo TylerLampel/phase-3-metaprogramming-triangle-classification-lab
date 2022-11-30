7bada8ccf69fbd1d59d20f3a2828b996f1d7dadf
Tyler Lampel <lampel13@gmail.com>
2022-11-30 13:08:26 -0500

    commit tree 71c42b26c49093b4d00b19bb9ce4d6fe917a6d8e

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  
+  attr_accessor :a, :b, :c
+
+  def initialize(a, b , c)
+    @a = a
+    @b = b
+    @c = c
+  end
+
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b== c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  def sides_larger_than_zero?
+    [a, b, c].all?(&:positive?)
+  end
+
+  def triangle_inequality?
+    a + b > c && a + c > b && b + c > a
+  end
+
+  def validate_triangle
+    raise TriangleError unless sides_larger_than_zero? && triangle_inequality?
+  end
+
+  class TriangleError < StandardError
+  end
 end
